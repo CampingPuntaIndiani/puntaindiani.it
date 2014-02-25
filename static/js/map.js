36ecc883476e70a2d5207fffa200de2eba4c354d
Martin Brugnara <martin.brugnara@gmail.com>
2014-02-25 12:26:58 +0100

    improve map js

--- static/js/map.js
+++ static/js/map.js
@@ -1,41 +1,58 @@
-(function($){
-  var points = [],
-    org_w = 1278, org_h = 1578,
-    id_prefix = "map_elm_",
-    id_prefix_len = id_prefix.length,
-    url_base = '/static/img/camping_map/view/',
-    marks = $('#points div'),
-    view = $('#view');
+(function ($) {
+    "use strict";
+    var points = [],
+        org_w = 1278,
+        org_h = 1578,
+        id_prefix = "map_elm_",
+        id_prefix_len = id_prefix.length,
+        url_base = '/static/img/camping_map/view/',
+        marks = $('#points div'),
+        view = $('#view'),
+        img_map = $('#img_map'),
 
-  var build_points = function(){
+        build_points = function () {
 
-    var w = parseInt($('#img_map').css('width'));
-    var h = parseInt($('#img_map').css('height'));
+            var w = parseInt(img_map.css('width'), 10),
+                h = parseInt(img_map.css('height'), 10);
 
-    return $.map(points,  function(p) {
-      return '<div style="left:'+Math.round(w*p.x/org_w - 20)+'px; top:'+Math.round(h*p.y/org_h - 20)+'px;" id="'+id_prefix+p.id+'" />';
-    }).join('\r');
+            return $.map(points,  function (p) {
+                return ['<div style="left:',
+                    Math.round(w * p.x / org_w - 20),
+                    'px; top:',
+                    Math.round(h * p.y / org_h - 20),
+                    'px;" id="',
+                    id_prefix,
+                    p.id,
+                    '" />'].join('');
+            }).join('\r');
 
-  };
+        },
 
-  var click = function(ev) {
-    var e = $(ev.target);
-    var img_url = e.attr('id').substr(id_prefix_len);
-    view.removeAttr('src');
-    $('#points div[selected]').removeAttr('selected');
-    e.attr('selected', 'selected');
-    view.attr('src', url_base+img_url+'.jpg');
-  };
+        click = function (ev) {
+            var e = $(ev.target),
+                id = e.attr('id'),
+                img_url = id.substr(id_prefix_len),
+                selected = $('#points div[selected]');
+            
+            if (!selected || selected.attr('id') === e.attr('id')) {
+                return;
+            }
+            
+            view.removeAttr('src');
+            selected.removeAttr('selected');
+            e.attr('selected', 'selected');
+            view.attr('src', url_base + img_url + '.jpg');
+        };
 
-  $(function(){
-    $.getJSON('/static/json/map.js', function(json){
-      points = json;
+    $(function () {
+        $.getJSON('/static/json/map.js', function (json) {
+            points = json;
 
-      $(window).on('resize', function(){
-        $('#points').html(build_points());
-      }).resize();
-      $('#points').delegate('div', 'click', click);
-      $('#'+id_prefix+points[0].id).click();
+            $(window).on('resize', function () {
+                $('#points').html(build_points());
+            }).resize();
+            $('#points').delegate('div', 'click', click);
+            $('#' + id_prefix + points[0].id).click();
+        });
     });
-  });
-})(window.jQuery);
+}(window.jQuery));
