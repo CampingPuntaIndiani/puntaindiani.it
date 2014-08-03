97c384c689664ce86d797f5574c155483f8ded39
Martin Brugnara <martin.brugnara@gmail.com>
2014-08-03 16:51:29 +0200

    Try using swiftmailer to use google smtp srv

--- libs/Mail.php
+++ libs/Mail.php
@@ -4,6 +4,9 @@
     // Contact : github.com/Martin-Brugnara
     //-------------------------------------
 
+
+require_once 'swift/lib/swift_required.php';
+
     class Mail
     {
         public function __construct($message, $to, $from='PHP', $subject='(No subject)')
@@ -23,8 +26,21 @@
                 '',
                 ''), "\r\n");
 
+            $transport = Swift_SmtpTransport::newInstance('smtp.googlemail.com', 465, "ssl")
+              ->setUsername($mail_user)
+              ->setPassword($mail_pass);
+
+            $mailer = Swift_Mailer::newInstance($transport);
+
+            $message = Swift_Message::newInstance($this->subject)
+              ->setFrom(array($from => $from))
+              ->setTo(array($to))
+              ->setBody($body);
+
+            return $mailer->send($message);
+
             #mail($to, $subject, $message, $headers); // Tecnodata
-            return mail($this->to, '=?UTF-8?B?'.base64_encode($this->subject).'?=', $this->message, $headers, "-f".$this->from); // Aruba
+            #return mail($this->to, '=?UTF-8?B?'.base64_encode($this->subject).'?=', $this->message, $headers, "-f".$this->from); // Aruba
         }
     }
 ?>
